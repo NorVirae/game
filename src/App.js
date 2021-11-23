eb8f0a698139e7802b4c76daef76b07dd4fa789c
NorVirae <norbertmbafrank@gmail.com>
2021-11-23 13:35:30 +0100

    trying to fix scroll

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import logo from './assets/images/logo.png';
 import bird from './assets/images/birds2.png';
 
 function App() {
+
+  const num = [1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17];
   return (
     
       <>
@@ -93,87 +95,55 @@ function App() {
 
         {/* All navs end here */}
 
-        <section className="collections">
+        <section className="collections grid grid-card-3">
 
-          <div className="container grid grid-3 gap-1">
-            <div className="card flex">
-                <img src={bird} className="logo"></img>
-                <div className="card-body">
-                    <div className="flex flex-column">
-                      <h4>C.spawn</h4>
-                      <p className="bar"></p>
-                      <div className="flex">
-                        <button className="btn btn-spawn"><i class="fas fa-golf-ball"></i>balls</button>
-                        <button className="btn btn-spawn">balls</button>
-                        <button className="btn btn-spawn">balls</button>
-                      </div>  
-
-                      
-                      </div>
-                    </div>
-              </div>
-
-              <div className="card flex">
-                <img src={bird} className="logo"></img>
-                <div className="card-body">
-                    <div className="flex flex-column">
-                      <h4>C.spawn</h4>
-                      <p className="bar"></p>
-                      <div className="flex">
-                        <button className="btn btn-spawn"><i class="fas fa-golf-ball"></i>balls</button>
-                        <button className="btn btn-spawn">balls</button>
-                        <button className="btn btn-spawn">balls</button>
-                      </div>  
-
-                      </div>
-                    </div>
-              </div>
-
-              <div className="card flex">
-                <img src={bird} className="logo"></img>
-                <div className="card-body">
-                    <div className="flex flex-column">
-                      <h4>C.spawn</h4>
-                      <p className="bar"></p>
-                      <div className="flex">
-                        <button className="btn btn-spawn"><i class="fas fa-golf-ball"></i>balls</button>
-                        <button className="btn btn-spawn">balls</button>
-                        <button className="btn btn-spawn">balls</button>
-                      </div>  
-
-                      </div>
-                    </div>
-
-                    
-
-              </div>
-
-
-              <div className="card flex">
-                <img src={bird} className="bird"/>
-
-                <div className="card-body flex flex-column justify-content-center align-items-center">
-                    
-                      <h4>C.spawn</h4>
-                      <p className="bar">.</p>
-                      <div className="flex">
-                
-                        <button className="btn btn-spawn"> <img src={bird} className="bird"/>balls</button>
-                        <button className="btn btn-spawn transparent">.</button>
-                        <button className="btn btn-spawn transparent">balls</button>
-
-                      </div>
-                    </div>
-                    <p className="flex flex-column justify-content-center align-items-flex-end">
-                      <h2>=0.518</h2>
-                        <h4>$506</h4>
-                      </p>
-              </div>
+          <div>
               
+            </div>
 
+          <div className="card-collections grid grid-3 gap-1">
+
+          {/* bEGINING OF CARD */}
+        {num.map((cd)=>{
+
+         return  (<div className="card flex">
+                      <img src={bird} className="bird"/>
+
+                      <div className="card-body flex flex-column justify-content-center align-items-center">
+                          
+                            <h4>C.spawn</h4>
+                            <p className="bar">.</p>
+                            <div className="flex">
+                      
+                              <button className="btn btn-spawn"> <img src={bird} className="bird"/>balls</button>
+                              <button className="btn btn-spawn transparent">.</button>
+                              <button className="btn btn-spawn transparent">balls</button>
+
+                            </div>
+                          </div>
+                          <p className="flex flex-column justify-content-center align-items-flex-end">
+                            <h2>=0.518</h2>
+                              <h4>$506</h4>
+                            </p>
+              </div>)
+
+              })}
+
+          {/* END OF CARD */}
 
               
           </div>
+
+        
+
+
+          <div className="flex flex-column justify-content-space-between align-items-flex-end">
+          <p><i class="fas fa-caret-circle-up fa-2x"></i></p>
+
+              <p><i class="fas fa-caret-circle-down fa-2x"></i></p>
+
+            </div>
+
         </section>
 
 
